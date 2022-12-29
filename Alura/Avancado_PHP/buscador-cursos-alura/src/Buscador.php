81d4b116629b7a625133d15f749adab01400c348
Lucas <lucasdamata01@hotmail.com>
2022-12-29 09:06:20 -0300

    Testes de integridade
    PHPUnit, Phan e PHPCS

--- Alura/Avancado_PHP/buscador-cursos-alura/src/Buscador.php
+++ Alura/Avancado_PHP/buscador-cursos-alura/src/Buscador.php
@@ -1,19 +1,23 @@
 <?php
+
 namespace Alura\BuscadorDeCursos;
 
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
-class Buscador{
+class Buscador
+{
     private $httpCliente;
     private $crawler;
 
-    public function __construct(ClientInterface $httpCliente, Crawler $crawler){
+    public function __construct(ClientInterface $httpCliente, Crawler $crawler)
+    {
         $this->httpCliente = $httpCliente;
         $this->crawler = $crawler;
     }
 
-    public function buscar(string $url): array{
+    public function buscar(string $url): array
+    {
         $resposta = $this->httpCliente->request('GET', $url);
 
         $html = $resposta->getBody();
@@ -22,11 +26,10 @@ class Buscador{
         $elementosCursos = $this->crawler->filter('span.card-curso__nome');
         $cursos = [];
 
-        foreach ($elementosCursos as $elemento){
+        foreach ($elementosCursos as $elemento) {
             $cursos[] = $elemento->textContent;
         }
 
         return $cursos;
     }
 }
-?>
